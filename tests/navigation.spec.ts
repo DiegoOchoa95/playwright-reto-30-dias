9ae5add01127807a03dd53d707670296c9db7f68
Diego <diegoochoaorellana@gmail.com>
2026-07-24 22:23:28 -0500

    Reto 03 completado ++

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -3,15 +3,15 @@ import {expect, test} from '@playwright/test'
 test('check left menu options', async({page}) => {
 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    await page.getByRole('textbox',{name:/Username|Nombre de usuario/}).fill('Admin') //se modifico para que permita ubicar los elementos en español e ingles
-    await page.getByRole('textbox',{name:/Password|Contraseña/}).fill('admin123')
+    await page.getByRole('textbox',{name:/username|Nombre de usuario/}).fill('Admin') //se modifico para que permita ubicar los elementos en español e ingles
+    await page.getByRole('textbox',{name:/password|Contraseña/}).fill('admin123')
     await page.getByRole('button',{name:/Login|Ingresar/}).click()
 
     await expect(page.getByRole('link',{name:'Admin'})).toBeVisible()//valida que ingresaste a la pagina
 
     const leftMenuItems = page.getByLabel('Sidepanel').getByRole('listitem')//capturo todos los valores del menu
     const currentMenuItemsCount = await leftMenuItems.count()//variable almacena la cantidad de items
-    console.log('cantidad de items en el menu son ', currentMenuItemsCount)
+    console.log('cantidad de items en el menu: ', currentMenuItemsCount)
 
     const currentMenuItems: String[]=[]
 
@@ -40,6 +40,12 @@ test('check left menu options', async({page}) => {
 
     expect(currentMenuItems).toEqual(expectMenuItems)
 
-    
+    //Tarea validar que el primer elemento es admin 
+    expect(currentMenuItems[0]).toBe('Admin')//si colocas otro valor cae esta validacion
 
+    if (currentMenuItems[0] === 'Admin') {
+        console.log('✅ Validación correcta: el primer menú es Admin');
+    } else {
+        console.log('❌ Validación incorrecta: el primer menú NO es Admin');
+    }
 })
